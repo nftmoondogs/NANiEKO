1abb8d02e0bc68469e9b8f05126666c95e30df4e
nftmoondogs <nftmoondogs@gmail.com>
2024-09-13 01:35:12 +0530

    newcommit

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
             <div className="">
               <Button size="xl" className="w-full font-bold" variant="brand">
                 <a
-                  href="https://sunpump.meme"
+                  href="https://dexscreener.com/moonshot"
                   target="_blank"
                   rel="noreferrer"
                   className="pb-1 dark:text-zinc-800 text-zinc-100"
