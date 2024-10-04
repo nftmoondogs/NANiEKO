ed2a7340e599b9b3c9b4705f000fe024327844fc
nftmoondogs <nftmoondogs@gmail.com>
2024-10-04 15:16:41 +0530

    newcommit

--- app/page.tsx
+++ app/page.tsx
@@ -15,24 +15,24 @@ export default function Home() {
             <span
               className={`-mt-14 inline-block text-[64px] font-bold text-black dark:text-fuchsia-600`}
             >
-              $NANiEKO Token on MoonShot🌙
+              $NANiEKO Token on Pump.Fun💊
             </span>
             <p className="pb-6 font-medium">
             Hi! my name is Nanie, I am a solo Solana Meme Token creator, an independent 2D animator. I use my magic touch to bring images to life. And I present to you $NANIEKO the cutest CAT token on Solana!
             </p>
             <p className="pb-6 font-medium">
-            Instead of having a PreSale we decided to launch using MoonShot
+            Instead of having a PreSale we decided to launch using Pump.Fun💊
             </p>
 
             <div className="">
               <Button size="xl" className="w-full font-bold" variant="brand">
                 <a
-                  href="https://dexscreener.com/moonshot"
+                  href="https://pump.fun/board"
                   target="_blank"
                   rel="noreferrer"
                   className="pb-1 dark:text-zinc-800 text-zinc-100"
                 >
-                  Buy From MoonShot🌙
+                  Buy From Pump.Fun💊
                 </a>{" "}
               </Button>
             </div>
