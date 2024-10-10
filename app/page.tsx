e5c01f17ef9e46d1cb698d072d3a98be10cf6ca5
nftmoondogs <nftmoondogs@gmail.com>
2024-10-10 16:43:08 +0530

    newcommit

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
             <div className="">
               <Button size="xl" className="w-full font-bold" variant="brand">
                 <a
-                  href="https://dexscreener.com/solana/6Z7X7ZChSPavEWtBcPDy1X4UWrqPcKKsBuVNmrigLYUE"
+                  href="https://dexscreener.com/moonshot/"
                   target="_blank"
                   rel="noreferrer"
                   className="pb-1 dark:text-zinc-800 text-zinc-100"
